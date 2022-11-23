08385b87662327f7d1ac647ba7caa9b3dc0c7557
You Matsumoto <youmts22@gmail.com>
2022-11-23 16:57:46 +0900

    feat: show loop_count

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ use langtons_ant::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::Point;
+use sdl2::rect::{Point, Rect};
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
-pub const SKIP_RENDER_FRAME: u32 = 1;
+pub const SKIP_RENDER_FRAME: u32 = 10;
 pub const CANVAS_SCALE: u32 = 4;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    // let ttl_context = sdl2::ttf::init().unwrap();
-    // let font = ttl_context.load_font("Sans.ttf", 24).unwrap();
+    let ttl_context = sdl2::ttf::init().unwrap();
+    let font = ttl_context.load_font("OpenSans-Regular.ttf", 24).unwrap();
 
     let window = video_subsystem
         .window(
@@ -43,10 +43,11 @@ pub fn main() {
         scene.work();
 
         if scene.loop_count() % SKIP_RENDER_FRAME == 0 {
-            render(&mut canvas, scene.field());
+            clear(&mut canvas);
+            render_scene(&mut canvas, scene.field());
+            render_information(&mut canvas, &font, scene.loop_count());
             canvas.present();
         }
-        // render_information(&mut canvas, &font, scene.loop_count());
 
         for event in event_pump.poll_iter() {
             match event {
@@ -64,10 +65,29 @@ pub fn main() {
     }
 }
 
-fn render(canvas: &mut Canvas<Window>, field: &Field) {
+fn render_information(canvas: &mut Canvas<Window>, font: &sdl2::ttf::Font, loop_count: u32) {
+    let text = format!("{}", loop_count);
+    let white = Color::RGB(255, 255, 255);
+    let surface = font.render(&text).solid(white).unwrap();
+    let texture_creator = canvas.texture_creator();
+    let texture = surface.as_texture(&texture_creator).unwrap();
+    let scale = 2;
+    
+    let rect = Rect::new(0, 0, surface.width() / scale, surface.height() / scale);
+    canvas.copy(&texture, None, rect).unwrap();
+
+    // canvas.with_texture_canvas(&mut texture, |canvas| {
+    //     let rect = Rect::new(0, 0, 100, 20);
+    //     canvas.draw_rect(rect).unwrap();
+    // }).unwrap();
+}
+
+fn clear(canvas: &mut Canvas<Window>) {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
+}
 
+fn render_scene(canvas: &mut Canvas<Window>, field: &Field) {
     let mut points = vec![];
 
     // TODO: draw only diffs
@@ -83,3 +103,4 @@ fn render(canvas: &mut Canvas<Window>, field: &Field) {
     canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.draw_points(&points[..]).unwrap();
 }
+
